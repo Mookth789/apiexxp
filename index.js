a4efe6088fecfb73d170d8ea8a1de427666c018b
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-05 05:09:18 +0700

    3 files modified, 8 added

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+const cors = require('cors');
 
+
+app.use(cors())
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
@@ -10,5 +13,27 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`mY app listening at http://localhost:${port}`);
 });
+
+
+const morgan = require('morgan')
+const bodyParse = require('body-parser')
+
+const connectDB = require('./Config/db')
+
+
+const { readdirSync } = require('fs')
+// const productRouters = require('./Routes/product')
+// const authRouters = require('./Routes/auth')
+
+
+connectDB()
+
+app.use(morgan('dev'))
+app.use(bodyParse.json({ limit: '10mb' }))
+
+// Route 3
+readdirSync('./Routes')
+    .map((r) => app.use('/api', require('./Routes/' + r)))
+
